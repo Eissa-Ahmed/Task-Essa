d3e1c9ae6230301ee0c3e0b8e363cb44edbc33fb
Eissa Ahmed <essa1998ahmed@gmail.com>
2023-08-18 07:55:16 +0200

    Add project files.

---
+++ Task Essa/Classes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Employee : PersonAll, PersonM_E
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public decimal salary { get; set; }
+
+        public void absense()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void attendance(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal getSalary(double baseSalary, double deduction, double bouns)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    internal class NotEmployee : PersonAll
+    {
+        public int id { get ; set ; }
+        public string name { get; set; }
+        public string address { get; set; }
+
+        public void eat() { }
+        public void drink() { }
+    }
+
+    internal class Manager : PersonAll, PersonM_E
+    {
+
+        decimal bouns { get; set; }
+        public decimal salary { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+
+        public void hire() { }
+        public void fire() { }
+
+        public decimal getSalary(double baseSalary, double deduction, double bouns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void absense()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void attendance(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task Essa/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal interface PersonAll
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string address { get; set; }
+    }
+    internal interface PersonM_E
+    {
+        decimal salary { get; set; }
+
+        public decimal getSalary(double baseSalary, double deduction, double bouns);
+
+
+        public void absense();
+        public void display();
+        public void attendance(string name);
+    }
+}
